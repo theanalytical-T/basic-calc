0ef8d7778ec521a8a760c4b06a05550d6459caa4
Tiara Brunt <53661450+tiara143@users.noreply.github.com>
2021-10-08 20:26:24 -0500

    10/8/2021
    Added the onclick functions and some sass elements.

--- Basic Calculator/dist/app.js
+++ Basic Calculator/dist/app.js
@@ -1,5 +1,31 @@
 let num1 = 8
 let num2 = 2
+let sum = document.getElementById("sum-el")
 
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
+
+function add() {
+    // console.log("hello there")
+    let result = num1 + num2
+    sum.textContent = "Sum: " + result
+
+}
+
+function subtract() {
+    let result = num1 - num2
+    sum.textContent = "Sum: " + result
+
+}
+
+function divide() {
+    let result = num1 / num2
+    sum.textContent = "Sum: " + result
+
+}
+
+function multiply() {
+    let result = num1 * num2
+    sum.textContent = "Sum: " + result
+
+}
